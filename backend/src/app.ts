e4b543de37a6e2004938ee00affd1fc066e26df2
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-09-07 16:32:47 -0300

    feat: inicia setup inicial

---
+++ backend/src/app.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+
+class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+
+    this.config();
+    this.routes();
+
+    this.app.get('/', (req, res) => res.json({ ok: true }));
+  }
+
+  private config():void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(express.json());
+    this.app.use(accessControl);
+  }
+
+  private routes(): void {
+    // Aqui você deve importar os arquivos de rotas
+  }
+
+  public start(PORT: string | number): void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+}
+
+export { App };
+
+// Essa segunda exportação é estratégica, e a execução dos testes de cobertura depende dela
+export const { app } = new App();

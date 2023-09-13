f57855fca836eeeeffd662e6d8f376a88ba68c6c
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-09-13 16:27:56 -0300

    Feat: Foi criado as interfaces das models para a realização das tipagens nas mesmas

---
+++ backend/src/interfaces/IFavorite.ts
@@ -0,0 +1,5 @@
+export default interface IFavorite {
+  id: number;
+  userId: number;
+  recipe: string;
+}
---
+++ backend/src/interfaces/IRecipe.ts
@@ -0,0 +1,6 @@
+export default interface IRecipe {
+  id: number;
+  name: string;
+  instructions: string;
+  categoryId: number;
+}
